84304fae536ec0be3db57930d954ba34bcb358f1
thsigit <th.sigit@gmail.com>
2026-08-26 11:41:45 +0800

    refactor: merge common/packages/ into single default.nix

--- common/packages/default.nix
+++ common/packages/default.nix
@@ -4,15 +4,70 @@
 { config, lib, pkgs, ... }:
 
 {
-  imports = [
-    ./programs.nix
-    ./bash.nix
-    ./tmux.nix
-    ./vim.nix
-    ./git.nix
-    ./fzf.nix
-    ./wireshark.nix
-  ];
+  programs.bash = {
+    promptInit = ''
+      export PS1="\[\e[1;36m\]🏠 HOMELAB \[\e[0m\]\w (\u) \$ "
+    '';
+    completion = {
+      enable = true;
+      package = pkgs.bash-completion;
+    };
+  };
+
+  programs.fzf = {
+    keybindings = true;
+    fuzzyCompletion = true;
+  };
+
+  programs.git = {
+    enable = true;
+    config = {
+      init.defaultBranch = "main";
+    };
+  };
+
+  programs.tmux = {
+    enable = true;
+    aggressiveResize = true;
+    historyLimit = 10000;
+    clock24 = true;
+    extraConfig = ''
+      set -g @continuum-restore on
+      set -g @continuum-save-interval 60
+      set -g @resurrect-capture-pane-contents on
+      set -g @resurrect-processes ssh
+      set -g @thumbs-key t
+      set -g @thumbs-unique enabled
+      set -g @tmux_power_theme gold
+      set -g default-terminal screen-256color
+      set -as terminal-features ,xterm-256color:RGB
+    '';
+    newSession = true;
+    plugins = with pkgs.tmuxPlugins; [
+      resurrect continuum tmux-fzf extrakto session-wizard yank
+      tmux-thumbs cpu power-theme sensible
+    ];
+  };
+
+  programs.vim = {
+    enable = true;
+    defaultEditor = true;
+  };
+
+  programs.wireshark = {
+    enable = true;
+    package = lib.mkDefault pkgs.tshark;
+  };
+
+  programs.bandwhich.enable = true;
+  programs.htop.enable = true;
+  programs.iftop.enable = true;
+  programs.iotop.enable = true;
+  programs.tcpdump.enable = true;
+  programs.traceroute.enable = true;
+  programs.ydotool.enable = true;
+
+  powerManagement.cpuFreqGovernor = "performance";
 
   environment.systemPackages = with pkgs; [
     bc rink
